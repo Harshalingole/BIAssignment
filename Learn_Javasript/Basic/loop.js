e7dc5cc97e4bd5e1f6d8f1ab11a6f169374833b2
Harshalingole <harshal.ingole21@gmail.com>
2021-12-25 17:43:24 +0530

    loop concept in js file

---
+++ Learn_Javasript/Basic/loop.js
@@ -0,0 +1,40 @@
+//For loop
+// for(let i = 0; i <10; i++) {
+//     console.log("Namestey World")
+// }
+
+//while loop
+// let i = 0;
+// while(i<10) {
+//     console.log("Namstey World")
+//     i++
+// }
+
+//Do while loop
+// let j = 1;
+// do {
+//     console.log(j)
+//     j++
+// }
+// while(j < 5) 
+
+//For in loop
+   //objects
+let animal = {
+    name: "Zebra" ,
+    leg: 4
+};
+for(let key in animal){
+    console.log(key, animal[key]);
+}
+  //Arrays
+  let names = ["Rahul", "Neha", "Aman", "Rishabh"];
+  for (let index in names){
+      console.log(index, names[index]);
+  }
+
+//For off loop-- using array
+for(let name of names){
+    console.log(name)
+}
+
