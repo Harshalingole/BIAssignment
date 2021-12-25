a99156dbea024c9c13b2d9e1da7f148123755c61
Harshalingole <harshal.ingole21@gmail.com>
2021-12-25 19:42:09 +0530

    Function in javasript-Basic

---
+++ Learn_Javasript/Basic/function.js
@@ -0,0 +1,23 @@
+// Maggi cooking recipe
+function cookMaggi(maggi, pani, tapeli) {
+    console.log("Your maggi will be ready in" 
+        + maggi * 2 
+        + "minutes"
+        +"and ingredients used are:-"
+        + maggi + "Maggi"
+        + pani + "cups of water"
+        +"using"+tapeli +"pan")
+}
+cookMaggi(4, 8 , 1)
+
+//Make Sandwich
+let bread1 = prompt("Which bread would you like to have:-")
+let veggies1 = prompt("Which Veggies would you like to have:-")
+let Sauce1 = prompt("Which Sauce would you like to have:-")
+
+function makeSandwich(bread, veggies, sauce) {
+    let sandwich = bread + " " + veggies + " " + sauce +" " +"sandwich is ready" ;
+    return sandwich;
+}
+let vegSandwich = makeSandwich(bread1, veggies1, Sauce1);
+console.log(vegSandwich);
