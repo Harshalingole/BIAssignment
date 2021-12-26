f9f5a0cc3e8cac0fe149d4f7d92130376dc33346
Harshalingole <harshal.ingole21@gmail.com>
2021-12-26 17:05:31 +0530

    operators in js

---
+++ Learn_Javasript/Basic/operators.js
@@ -0,0 +1,21 @@
+// Operators
+let x=2,  y=3;
+
+// Assignmnet Operators
+z= x + y;
+z= x - y;
+z= x * y;
+z= x/y;
+z= x % y; //modulas(Division Reminder)
+z = x**y;
+z = x&&(x=y) //not understand------
+z
+z = x||(x=y) //not understand-----
+z
+z = x??(x=y) //not understand----
+z
+// String Operators
+
+
+
+
