51ed6394eeefa946e626cbbf6c32ee60bedc5f5d
Harshalingole <harshal.ingole21@gmail.com>
2021-12-27 15:55:12 +0530

    javasript Practice Folder added

---
+++ Learn_Javasript/Exercies_Practice/Js_Hero/string.js
@@ -0,0 +1,13 @@
+//indexof(of) string
+function secondIndexOf(s1, s2) {
+    let first = s1.indexOf(s2);
+    return  s1.indexOf(s2, first  + 1)
+  }
+  console.log(secondIndexOf('White Rabbit', 'it'))
+
+//string:substr()
+function firstWord(s1) {
+    let spaceindex = s1.indexOf(' ');
+    return s1.substr(0, spaceindex)
+}
+console.log(firstWord('see and stop'))
