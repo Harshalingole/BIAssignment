d6ebc0bf7bdca68f9aa7d57ca5cb220bb442db86
Harshalingole <harshal.ingole21@gmail.com>
2021-12-27 20:15:04 +0530

    number

---
+++ Learn_Javasript/Exercies_Practice/Js_Hero/number.js
@@ -0,0 +1,33 @@
+//Number 
+// Write a function add that takes two numbers and returns their sum.
+// Example: add(1, 2) should return 3.
+function add(s1, s2) {
+    return s1 + s2 
+}
+
+//Increment a number
+let x = 3;
+x++;
+x = x * 2;
+x--;
+console.log(x);
+
+// Write a function toFahrenheit that converts a temperature from Celsius to Fahrenheit.
+function toFahrenheit(C) {
+    return F = 1.8 * C + 32.
+}
+console.log(toFahrenheit(0))
+
+//Modulo
+// Write a function onesDigit that takes a natural number and returns the ones digit of that number.
+function onesDigit(h1) {
+    return h1%10
+}
+console.log(onesDigit(2674))
+
+//Parenthesis
+//Write a function mean that takes 2 numbers and returns their mean value.
+function mean(h1, h2) {
+    return (h1 + h2) / 2
+}
+console.log(mean(1, 2))
