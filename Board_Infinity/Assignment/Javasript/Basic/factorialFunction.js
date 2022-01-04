3a5f2f0e2efaa4bd2ae72afc03d6e3225a49848c
Harshalingole <harshal.ingole21@gmail.com>
2022-01-04 20:02:25 +0530

    dought asked soultion added

--- Board_Infinity/Assignment/Javasript/Basic/factorialFunction.js
+++ Board_Infinity/Assignment/Javasript/Basic/factorialFunction.js
@@ -5,4 +5,28 @@ function factorial(n) {
     let ans = n *b;
     return ans
 }
-console.log(factorial())
+// console.log(factorial(2))
+function factorial(n) {
+    for (n=0; n>0 ; n--) {
+        return
+}
+
+//Finding max number
+
+function maxnum(a, b) {  // finding max number using group of variable number
+    return Math.max(a, b);
+}
+console.log(maxnum("5", "6"));
+
+var num = [0,1,2,3,4,5,6,7,8,9]
+for(let name of num) {
+    console.log(num);
+    let a = num;
+    a
+}
+function maxnum1(num) {
+    for(let name of num) {
+        console.log(num);
+        let a = num;
+    }
+} 
