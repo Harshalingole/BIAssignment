1427d81be4997f8706ca5a4c49f18ef4c05ddce0
Harshalingole <harshal.ingole21@gmail.com>
2022-01-05 16:03:26 +0530

    Arrays in Js - W3

---
+++ Learn_Javasript/Basic/Array_W3.js
@@ -0,0 +1,19 @@
+// Using splice() to Remove Elements
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let x = fruits.splice(2, 2, "Lemon", "Kiwi");;
+x
+fruits
+
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+let x1 = fruits1.splice(0, 1);
+x1
+fruits1
+
+const fruits2 = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits2.slice(1);
+citrus
+fruits2
+y = fruits2.join(" ")
+y
+
+//JavaScript Sorting Arrays
